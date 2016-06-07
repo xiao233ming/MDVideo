63ee177658b7ba0d4511ab7ca28fb903578e76d1
arthar <929838354@qq.com>
2016-06-07 20:30:15 +0800

    add a playerActivity

---
+++ app/src/main/java/com/artharyoung/mdvideo/PlayerModule/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.artharyoung.mdvideo.PlayerModule;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.artharyoung.mdvideo.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
