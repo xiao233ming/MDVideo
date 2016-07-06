6e62a15f1025ada0905dc8b6c19a80f8e8c472a6
arthar <929838354@qq.com>
2016-07-06 17:47:24 +0800

    修改主页面框架

--- app/src/main/java/com/studyjams/mdvideo/MainActivity.java
+++ app/src/main/java/com/studyjams/mdvideo/MainActivity.java
@@ -1,53 +1,63 @@
 package com.studyjams.mdvideo;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
+
+import com.studyjams.mdvideo.Adapter.MainPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+    private ViewPager mViewpager;
+    private List<String> mData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initStatusBar();
+        initData();
+        initView();
+    }
 
+    private void initData(){
+        mData = new ArrayList<>();
+        mData.add(getResources().getString(R.string.menu_video_local));
+        mData.add(getResources().getString(R.string.menu_video_history));
+        mData.add(getResources().getString(R.string.menu_video_setting));
+    }
+
+    private void initView(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
-        if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
-            final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.content_main, com.studyjams.mdvideo.Fragment.VideoListFragment.newInstance(""), TAG);
-            ft.commit();
-        }
-    }
-
-    private void initStatusBar() {
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.main_view_table);
+        mViewpager = (ViewPager)findViewById(R.id.main_view_pager);
+        MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager(),mData);
+        mViewpager.setAdapter(mainPagerAdapter);
+        tabLayout.setupWithViewPager(mViewpager);
     }
 
     @Override
@@ -85,21 +95,22 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch(item.getItemId()){
 
-        } else if (id == R.id.nav_slideshow) {
+            case R.id.menu_video_display:
 
-        } else if (id == R.id.nav_manage) {
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.menu_video_share:
 
-        } else if (id == R.id.nav_send) {
+                break;
+            case R.id.menu_video_send:
 
+                break;
+            default:
+
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
